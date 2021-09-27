19f8ef9e352d4b257208279b25c1429de2c1f979
peteralabi <peteralabi@Peters-MacBook.local>
2021-09-26 18:34:33 -0600

    added the popup inheritance class and open by default

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '500')
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
-
+from kivy.uix.popup import Popup
+class FileChoosePopUp(Popup):
+    pass
 class WORDCLOUD(AnchorLayout):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.file_chooser_popup = FileChoosePopUp()
+        self.file_chooser_popup.open()
     pass
 
 class MainApp(App):
